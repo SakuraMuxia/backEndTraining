852e2cc5d2df95df937ff0dcdfcccc1e45dccd93
yuluo <2216847528@qq.com>
2025-02-19 16:51:10 +0800

    🎉 添加SE功能测试

--- 001-JavaSE/SeProjectTest/Sakuna/src/com/yuluo/ForTest.java
+++ 001-JavaSE/SeProjectTest/Sakuna/src/com/yuluo/ForTest.java
@@ -1,2 +1,18 @@
-package com.yuluo;public class ForTest {
+package com.yuluo;
+
+public class ForTest {
+    public static void main(String[] args) {
+        for(int i=1;i <=5;i++){ // 外层循环 控制行数
+            // 左半部分
+            for(int j = 5; j >= i;j--){
+                System.out.print(" ");
+            }
+            // 右半部分
+            for(int j = 1;j <= (2 * i -1);j++){
+                System.out.print("*");
+            }
+            // 换行
+            System.out.println();
+        }
+    }
 }
