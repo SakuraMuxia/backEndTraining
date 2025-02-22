2460422e71bdb6dabc1e66b92d1df3cbb8e89c11
yuluo <2216847528@qq.com>
2025-02-22 11:35:45 +0800

    ✅ 更新JavaSE数组

--- 001-JavaSE/SeProjectTest/Sakuna/src/com/yuluo/YangHuiTri.java
+++ 001-JavaSE/SeProjectTest/Sakuna/src/com/yuluo/YangHuiTri.java
@@ -1,2 +1,28 @@
-package com.yuluo;public class YangHuiTri {
+package com.yuluo;
+
+public class YangHuiTri {
+    public static void main(String[] args) {
+        // 定义一个 6*6 的矩阵
+        int [][] yh = new int[6][6];
+        // 遍历 赋值
+        for (int i = 0; i < yh.length; i++) {
+            yh[i][0] = 1; // 每一行的第一列为1
+            yh[i][i] = 1; // 每一行的第i列为1
+            if(i > 1){ // 表示从第3行开始 赋值有具体的计算
+                for (int j = 1; j < i; j++) {
+                    yh[i][j] = yh[i-1][j] + yh[i-1][j-1];
+                }
+            }
+        }
+        // 遍历 打印
+        for(int i = 0;i < yh.length;i++){
+            for(int j = 6;j >= i;j--){
+                System.out.print(" "); // 打印一个倒三角形
+            }
+            for(int j = 0;j <= i;j++){  // 打印一个正三角形
+                System.out.print(yh[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
--- 001-JavaSE/SeProjectTest/Sakuna/src/com/yuluo/twoDimeArray.java
+++ 001-JavaSE/SeProjectTest/Sakuna/src/com/yuluo/twoDimeArray.java
@@ -1,2 +1,22 @@
-package com.yuluo;public class twoDimeArray {
+package com.yuluo;
+
+public class twoDimeArray {
+    public static void main(String[] args) {
+        // 定义一维数组
+        int [] arr1 = {1,2,3,4,5};
+        // 定义二维数组
+        int [][] arr2 = {{1,2,4},{16,17,18}};
+        // 打印二维数组中内层
+        System.out.println(arr2[0]); // [I@4554617c
+        System.out.println(arr2[0][2]); // 4
+        //
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.println(arr2[i]); // 全部是内存地址
+            for (int j = 0; j < arr2[i].length; j++) {
+                System.out.print(arr2[i][j] + "\t"); // 1	2	4
+            }
+            System.out.println();
+        }
+
+    }
 }
