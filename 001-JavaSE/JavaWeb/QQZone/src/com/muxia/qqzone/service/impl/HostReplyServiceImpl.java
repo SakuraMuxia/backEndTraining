2181f5e7e74a633a6709ea37936770ffd11f74a5
yuluo <2216847528@qq.com>
2025-10-23 17:54:50 +0800

    ✅ 已获取hostReply数据

--- 001-JavaSE/JavaWeb/QQZone/src/com/muxia/qqzone/service/impl/HostReplyServiceImpl.java
+++ 001-JavaSE/JavaWeb/QQZone/src/com/muxia/qqzone/service/impl/HostReplyServiceImpl.java
@@ -1,11 +1,13 @@
 package com.muxia.qqzone.service.impl;
 
+import com.muxia.qqzone.dao.HostReplyDao;
 import com.muxia.qqzone.pojo.HostReply;
 import com.muxia.qqzone.service.HostReplyService;
 
 public class HostReplyServiceImpl implements HostReplyService {
+    private HostReplyDao hostReplyDao;
     @Override
     public HostReply getHostReplyByReplyId(Integer replyId) {
-        return null;
+        return hostReplyDao.getHostReplyByReplyId(replyId);
     }
 }
